77c6d9434a4623ce5827a71b2ff9a415ae80a4aa
george <george@georges-MacBook-Pro-2.local>
2015-02-07 12:56:41 +1000

    separated sine wave class into separate file

--- SineWaver_Test1/AppDelegate.swift
+++ SineWaver_Test1/AppDelegate.swift
@@ -21,50 +21,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
        
         
-        class SineWave {
-            //properties
-            
-            var amplitudeWave :Double
-            var frequencyWave:Float
-            let periodWave:Float
-            var phase:Float
-            var valueArray = [Double]()
-            
-            
-            
-            //methods
-            
-            
-            init(){
-                
-                
-                amplitudeWave = 1.00
-                frequencyWave = 100.00
-                periodWave = 10.00
-                phase = 0.00
-                
-            
-                
-                var instantaneousAmpValue :Double
-                
-               for i in 0...359 {
-                    var degreeCount:Double = Double(i)
-                    instantaneousAmpValue = sin(degreeCount * pi / 180)
-                
-                
-                
-                    self.valueArray.append(instantaneousAmpValue)
-//                    
-//                    
-               }
-//                
-                
-            }
-            
-            
-        } // end of class definition
-        
-        
+               
       
         
         var sineWave1 = SineWave()

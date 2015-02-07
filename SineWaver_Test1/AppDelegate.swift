bf2d20cc368d5d04f018dd113156607a1d3a5423
george <george@georges-MacBook-Pro-2.local>
2015-02-07 10:53:49 +1000

    starting point commit

--- SineWaver_Test1/AppDelegate.swift
+++ SineWaver_Test1/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import Cocoa
 
+ let pi = M_PI
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
@@ -15,7 +17,63 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+       
+        
+       
+        
+        class SineWave {
+            //properties
+            
+            var amplitudeWave :Double
+            var frequencyWave:Float
+            let periodWave:Float
+            var phase:Float
+            var valueArray = [Double]()
+            
+            
+            
+            //methods
+            
+            
+            init(){
+                
+                
+                amplitudeWave = 1.00
+                frequencyWave = 100.00
+                periodWave = 10.00
+                phase = 0.00
+                
+            
+                
+                var instantaneousAmpValue :Double
+                
+               for i in 0...359 {
+                    var degreeCount:Double = Double(i)
+                    instantaneousAmpValue = sin(degreeCount * pi / 180)
+                
+                
+                
+                    self.valueArray.append(instantaneousAmpValue)
+//                    
+//                    
+               }
+//                
+                
+            }
+            
+            
+        } // end of class definition
+        
+        
+      
+        
+        var sineWave1 = SineWave()
+        
+        println("the period of \(sineWave1.frequencyWave)Hz is \(sineWave1.periodWave)ms")
+        println("\(sineWave1.valueArray)")
+        
+        
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {

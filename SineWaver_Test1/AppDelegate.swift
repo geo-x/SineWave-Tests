a6176736887a4b1aa3e4802828772fc1bdc83dbd
george <george@georges-MacBook-Pro-2.local>
2015-02-08 17:11:08 +1000

    this includes Mark Dalrymple's example of getting cg context and then uses it to draw a red test shape

--- SineWaver_Test1/AppDelegate.swift
+++ SineWaver_Test1/AppDelegate.swift
@@ -28,10 +28,52 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         println("the period of \(sineWave1.frequencyWave)Hz is \(sineWave1.periodWave)ms")
         println("\(sineWave1.valueArray)")
+       
         
         
         
-    }
+        
+        var currentContext : CGContext? {
+            get {
+                // The 10.10 SDK provdes a CGContext on NSGraphicsContext, but
+                // that's not available to folks running 10.9, so perform this
+                // violence to get a context via a void*.
+                // iOS can use UIGraphicsGetCurrentContext.
+                
+                let unsafeContextPointer = NSGraphicsContext.currentContext()?.graphicsPort
+                
+                if let contextPointer = unsafeContextPointer {
+                    let opaquePointer = COpaquePointer(contextPointer)
+                    let context: CGContextRef = Unmanaged.fromOpaque(opaquePointer).takeUnretainedValue()
+                    return context
+                } else {
+                    return nil
+                }
+            }
+            
+        }
+        
+        
+        println("\(currentContext)")
+        
+         //println("\(currentContext)")
+        
+        CGContextSetRGBFillColor (currentContext, 1.0, 1.0, 1.0, 1.0)
+        
+        
+        CGContextSetRGBFillColor(currentContext, 1, 0, 0, 1)
+        CGContextFillRect(currentContext, CGRectMake(0, 0, 100, 100))
+        CGContextFlush(currentContext)
+        
+    } // end of main loop
+    
+    
+    
+
+   
+    
+    
+    
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
